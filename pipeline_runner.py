2b9f8bd420a9644a38dae90c5cceee0fb89f920d
Brentani, Romano Fabiano (STUDENTS) <romano.brentani@students.unibe.ch>
2026-04-11 21:10:21 +0200

    added transformer

--- pipeline_runner.py
+++ pipeline_runner.py
@@ -1,7 +1,22 @@
 from data_processing import get_dataloaders
 import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import Subset
 
 class PipelineRunner:
+
+    train_transform_aug = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        transforms.ToTensor(),
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+
     def __init__(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         pass

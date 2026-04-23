7f18b303c089fcbc5f83b2daed9a51b92fa2609d
TheRBen12 <48603396+TheRBen12@users.noreply.github.com>
2026-04-23 18:28:55 +0200

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -27,3 +27,62 @@ def make_box_prediction(model, img_path: str, transformer, device):
     output = model(image)
 
   return output
+
+
+# Pipeline for detecting, cropping and classifying
+
+categories = {0: "short sleeve top", 1: "long sleeve top", 2: "short sleeve outwear",
+              3: "long sleeve outwear", 4: "vest", 5: "sling",
+              6: "shorts", 7: "trousers", 8: "skirt",
+              9:"sleeve dress", 10: "long sleeve dress", 11: "vest dress",
+              12: "sling dress"}
+
+def detect_crop_(image_path, detector, detector_transforms):
+    # 1. load image
+    orig_img = Image.open(image_path).convert("RGB")
+
+    # 2. Transorm the image and shift to device
+    img_tensor = detector_transforms(orig_img).to(device)
+
+    # 3. Faster R-CNN Prediction
+    print("start object detection...")
+
+    # set model in evaluation mode
+    detector.eval()
+
+    with torch.inference_mode():
+        predictions = detector([img_tensor])
+
+    # 4. Find box with highest score (x1, y1, x2, y2)
+    box = find_best_clothing_box(predictions)
+
+    if box is None:
+        print("No box found")
+        return orig_img, None, "No Detection", 0.0
+
+    x1, y1, x2, y2 = box
+    print(f"Bounding Box found: [{x1:.1f}, {y1:.1f}, {x2:.1f}, {y2:.1f}]")
+
+    # 5. Crop image accoring to found box (PIL uses x1, y1, x2, y2)
+    cropped_img = orig_img.crop((x1, y1, x2, y2))
+
+    # return the croped image
+    return cropped_img
+
+def predict(cropped_image, classifier, classifier_transforms):
+  # first create tensor of cropped image
+  cropped_image_tensor = classifier_transforms(cropped_image).unsqueeze(0).to(device)
+
+  # Make classification prediction
+  # 1. set model in evaluation model
+  classifier.eval()
+
+  with torch.inference_mode():
+    # 2. compute y logits
+    predictions = classifier(cropped_image_tensor)
+
+  probs = torch.softmax(predictions, dim=1)
+  prediction = torch.argmax(probs, dim=1).item()
+  prediction_label = categories[prediction]
+
+  return prediction, prediction_label
